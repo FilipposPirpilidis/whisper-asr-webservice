6c1b2198632d3db6df459cd620cbdc66a7795896
Filippos Pirpilidis <f.pirpilidis@gmail.com>
2025-04-30 20:03:55 +0300

    +auth

--- app/webservice.py
+++ app/webservice.py
@@ -7,6 +7,7 @@ from urllib.parse import quote
 import click
 import uvicorn
 from fastapi import FastAPI, File, Query, UploadFile, applications, Depends
+from app.dependencies.auth import verify_token
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import RedirectResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
